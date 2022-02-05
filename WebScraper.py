6cbcd927ccd7bca3b67a792937305391d348cbe1
Sp3ctroR3tro <76521319+Sp3ctroR3tro@users.noreply.github.com>
2022-02-05 13:22:49 -0500

    Update WebScraper.py
    Cleaned up the code now all text between <h2> </h2> tags will print minus the tags.

--- WebScraper.py
+++ WebScraper.py
@@ -1,6 +1,3 @@
-# pip install requests
-# pip install bs4
-# The two commands listed above installs the dependencies needed for this program to run. 
 import requests
 from bs4 import BeautifulSoup
 def main():
@@ -8,19 +5,14 @@ def main():
     response = requests.get(url)
 
     print(f"currently scraping: {url}")
-
-
     soup = BeautifulSoup(response.content, "html.parser")
     H2 = soup.find_all('h2')
 
-    print("Here are all of the <h2> headings.")
+    print("Here is the text of all <h2> headings tags :")
 
     for headings in H2:
-        page_headings = print(headings)
 
-
-
-
+        page_headings = print(headings.get_text())
 
 if __name__ == "__main__":
     main()
