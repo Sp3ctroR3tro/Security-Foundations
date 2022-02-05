e82fde2db5003f67895470c5c6f1ba842ac839a6
Sp3ctroR3tro <76521319+Sp3ctroR3tro@users.noreply.github.com>
2022-02-05 08:12:53 -0500

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,3 +1,23 @@
-cool_number = 12 + 30
-cool_number * 5
-print(cool_number)
+import requests
+from bs4 import BeautifulSoup
+def main():
+    url = "https://www.hardenwindows10forsecurity.com/"
+    response = requests.get(url)
+
+    print(f"currently scraping: {url}")
+
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    H2 = soup.find_all('h2')
+
+    print("Here are all of the <h2> headings.")
+
+    for headings in H2:
+        page_headings = print(headings)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
